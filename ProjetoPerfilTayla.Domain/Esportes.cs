bc0818678add2eda7bb1aede75cb051e1018ebf1
tayla <tayla.saab@aluno.senai.br>
2025-06-11 14:26:53 -0300

    Add clss esporte

--- ProjetoPerfilTayla.Domain/Esportes.cs
+++ ProjetoPerfilTayla.Domain/Esportes.cs
@@ -3,7 +3,7 @@
     public class Esportes 
     { 
         public int Id {get;set;}
-        public string Esporte {get;set;}
+        public string Modalidade {get;set;}
         public string Time {get;set;}
         public DateTime DiaDeJogo {get;set;}
         public string HoraDoJogo {get;set;}
